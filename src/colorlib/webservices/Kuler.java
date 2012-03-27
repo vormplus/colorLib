3c5c506e9913aa9b99c82dcd7af9766df61b8e6a
Jan Vantomme <jan@vormplus.be>
2012-03-27 15:21:10 +0200

    Basic version of Kuler class.

--- src/colorlib/webservices/Kuler.java
+++ src/colorlib/webservices/Kuler.java
@@ -27,22 +27,88 @@
 
 package colorlib.webservices;
 
-import processing.core.*;
+import java.util.ArrayList;
 
-public class Kuler {
+import processing.core.*;
+import colorlib.Palette;
+
+public class Kuler extends WebService {
 
 	protected PApplet p;
 	
 	private String key;
 	
-	public Kuler( final PApplet parent )
+	private String KULER_API_URL = "http://kuler-api.adobe.com/rss/";
+	
+	public Kuler( final PApplet parent, String _key )
 	{
 		p = parent;
+		key = _key;
+		
+		p.println( "KEY: " + key );
 		
 	}
-	
-/*	public Kuler setKey( String key )
+
+	public KulerPalette[] getHighestRated() // should return KulerPalette[]
 	{
-		this.key = key;		
-	} */
+		return makeKulerPalettes( "&listtype=rating", "get", null );
+	}
+	
+	private KulerPalette[] makeKulerPalettes( final String query, final String queryType, final String fileName ) // should return KulerPalette[]
+	{
+		
+		String url = new StringBuffer( KULER_API_URL ).append( queryType ).append( ".cfm?" )
+		                                              .append( query )
+		                                              .append( "&key=" + key )
+		                                              .toString();
+		
+		ArrayList<KulerPalette> palettes = new ArrayList<KulerPalette>();
+		
+		XML xml = p.loadXML( url );
+		// p.println( xml );
+		
+		if ( xml.getChild( "success" ) != null && xml.getChild("success").getContent().equals("false") ) {
+			
+			if ( DEBUG == true ) {
+				p.println( "The following error occurred while calling the Kuler API: " );
+				p.println( xml.getChild("error/errorText").getContent() );
+			}
+			
+		} else {
+			
+			XML[] themeItems = xml.getChildren( "channel/item/kuler:themeItem" );
+			
+			// p.println( themeItems );
+			
+			for ( int i = 0; i < themeItems.length; i++ ) {
+				
+				XML themeItem = themeItems[i];
+				XML[] themeSwatches = themeItem.getChildren( "kuler:themeSwatches/kuler:swatch/kuler:swatchHexColor" );
+				
+				int[] colors = new int[ themeSwatches.length ];
+				for ( int j = 0; j < themeSwatches.length; j++ ) {
+					colors[j] = PApplet.unhex( "FF" + themeSwatches[j].getContent() );
+				}
+				
+				KulerPalette kp = new KulerPalette( p, colors );
+				
+				// add author, id, tags, ... here ???
+				
+				palettes.add( kp );
+				
+			}
+			
+		}
+		
+		return (KulerPalette[]) palettes.toArray( new KulerPalette[ palettes.size() ] );
+		
+		//XML xml = getXML( url, fileName );
+		
+		
+/*		if ( DEBUG == true ) {	
+			p.println( "URL: " + url );
+		} */
+		
+		// return (KulerPalette[]) palettes.toArray();
+	}
 }
