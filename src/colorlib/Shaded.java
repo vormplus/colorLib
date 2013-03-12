c12e1d500eb6869396baaa4b593768c81aa463fc
Jan Vantomme <jan@vormplus.be>
2013-03-12 21:47:39 +0100

    Added Shaded class

---
+++ src/colorlib/Shaded.java
@@ -0,0 +1,31 @@
+package colorlib;
+
+import processing.core.PApplet;
+import colorlib.Palette;
+import colorlib.Swatch;
+
+public class Shaded extends Palette
+{
+	public Shaded( final PApplet parent )
+	{
+		super( parent );
+	}
+
+	public Shaded( final PApplet parent, final int color )
+	{
+		super( parent );
+	}
+	
+	public void setColor( final int color )
+	{
+		makeShaded( color );
+	}
+	
+	private void makeShaded( final int color )
+	{
+		for ( int i = 0; i < 6; i++ ) {
+			int c = p.lerpColor( color, p.color( 0 ), i * 0.2f );
+			Swatch swatch = new Swatch( p, c );
+		}
+	}
+}
