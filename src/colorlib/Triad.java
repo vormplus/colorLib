ddde45831f3b6160621eb0d3388640a63e5131a8
Jan Vantomme <jan@vormplus.be>
2013-03-12 21:32:01 +0100

    Added Triad class

---
+++ src/colorlib/Triad.java
@@ -0,0 +1,53 @@
+package colorlib;
+
+import processing.core.*;
+import colorlib.Palette;
+import colorlib.Swatch;
+
+public class Triad extends Palette
+{
+
+	private int angle;
+	
+	private int color;
+	
+	public Triad( final PApplet parent )
+	{
+		super( parent );
+		
+	}
+	
+	public Triad( final PApplet parent, final int color, final int angle )
+	{	
+		super( parent );
+	}
+
+	public void setColor( final int color )
+	{
+		
+		
+	}
+
+	public void setAngle( final int angle )
+	{
+		
+		
+	}
+
+	private void makeTriad()
+	{
+		angle = PApplet.constrain( angle, 0, 180 );
+		
+		swatches.add( new Swatch( p, color ) );
+		
+		Swatch secondSwatch = new Swatch( p, color );
+		secondSwatch.rotateRYB( angle );
+		swatches.add( secondSwatch );
+		
+		Swatch thirdSwatch = new Swatch( p, color );
+		thirdSwatch.rotateRYB( -angle );
+		swatches.add( thirdSwatch );
+		
+	}
+	
+}
